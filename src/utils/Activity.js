7e2dbf14f156003998ac76d77a674967afdcfce2
Feast <1353216996@qq.com>
2023-02-17 11:51:25 +0800

    活 定义

---
+++ src/utils/Activity.js
@@ -0,0 +1,36 @@
+import Request from "./Request";
+export class Activity {
+    constructor(id) {
+        this.id = id??null;
+        this.title = null;
+        this.titleColor = "#000";
+        this.logo = null;
+        this.background = null;
+        this.pictures = [];
+        this.pictureSpeed = null;
+        this.signSpeed = null;
+        this.createTime = null;
+    }
+    getFileForm() {
+        return Request.form({
+            logo: this.logo,
+            background: this.background,
+            pictures: this.pictures,
+        });
+    }
+    getDataQuery(){
+        return  {
+            title: this.title,
+            titleColor: this.titleColor,
+            pictureSpeed: this.pictureSpeed,
+            signSpeed: this.signSpeed,
+        }
+    }
+    static Default(){
+        let ret = new Activity();
+        ret.id = "123456789";
+        ret.title = "活动1";
+        ret.createTime = new Date();
+        return ret;
+    }
+}
