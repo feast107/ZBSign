f938ca487df81ee4308ec92f64e219ab424dc09d
Feast <1353216996@qq.com>
2023-03-07 18:12:04 +0800

    Update Activity.js

--- src/utils/Activity.js
+++ src/utils/Activity.js
@@ -14,17 +14,20 @@ export class Activity {
         this.backgroundUrl = "";
         this.pictureUrls = [];
 
-        this.pictureSpeed = "";
-        this.signSpeed = "";
+        this.pictureSpeed = "1x";
+        this.signSpeed = "1x";
         this.createTime = "";
         this.sharedLink = "";
 
+        this.rollEffect = "";
+        this.localSign = [];
     }
     getFileForm() {
         return Request.form({
             logo: this.logo,
             background: this.background,
             pictures: this.pictures,
+            localSign: this.localSign
         });
     }
     getDataQuery() {
@@ -33,12 +36,13 @@ export class Activity {
             titleColor: this.titleColor,
             pictureSpeed: this.pictureSpeed,
             signSpeed: this.signSpeed,
+            rollEffect: this.rollEffect,
         };
     }
     static Default() {
         let ret = new Activity();
         ret.id = "123456789";
-        ret.title = "南京孜博汇信息科技有限公司南京孜博汇信123";
+        ret.title = "南京孜博汇信息科技有限公司";
         ret.titleColor = "#faf";
         ret.createTime = new Date();
         ret.sharedLink = GUID.NewGuid();
