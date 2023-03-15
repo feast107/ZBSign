7fa4da81acb090a304336faa94cfb6baeb13fbf8
Feast <1353216996@qq.com>
2023-03-15 20:27:46 +0800

    Update Canvas.js

--- src/utils/Canvas.js
+++ src/utils/Canvas.js
@@ -1,8 +1,10 @@
 import { GUID } from "./Definition";
+import { Timer } from "./Format";
+import Request from "./Request";
 
 export const DotInfo = {
     width: 5600,
-    height: 7920,
+    height: 3960,
 };
 
 export class Dot {
@@ -47,6 +49,107 @@ export class Dot {
     static Move(x, y, address) {
         return new Dot(x, y, "move", address);
     }
+    static pageNum(startAddress, address, pageSize) {
+        let beginStrs = startAddress.split(".");
+        let pageStrs = address.split(".");
+        if (beginStrs[0] != pageStrs[0]) {
+            return -1;
+        }
+        let begin_C = Number(beginStrs[3]);
+        let begin_B = Number(beginStrs[2]);
+        let begin_A = Number(beginStrs[1]);
+
+        let page_C = Number(pageStrs[3]);
+        let page_B = Number(pageStrs[2]);
+        let page_A = Number(pageStrs[1]);
+
+        let page = -1;
+        switch (beginStrs[0]) {
+            //拓思德点阵
+            case "1713": {
+                page =
+                    ((page_A - begin_A) * 53 + (page_B - begin_B)) * 108 +
+                    (page_C - begin_C) +
+                    1;
+                break;
+            }
+            //拓思德点阵
+            case "1536": {
+                page =
+                    ((page_A - begin_A) * 73 + (page_B - begin_B)) * 108 +
+                    (page_C - begin_C) +
+                    1;
+                break;
+            }
+            //腾千里点阵
+            default: {
+                page = (page_B - begin_B) * 256 + (page_C - begin_C) + 1;
+                break;
+            }
+        }
+
+        if (page > 0 && page <= pageSize) {
+            return page;
+        } else return -1;
+    }
+    static pageAddress(startAddress, pageNum) {
+        let ret = "";
+        if (pageNum > 0) {
+            let strAry = startAddress.split(".");
+            for (let i = 0; i < pageNum; i++) {
+                let of = i;
+                let stA = Number(strAry[1]);
+                let stB = Number(strAry[2]);
+                let stC = Number(strAry[3]);
+                let numA = 108;
+                let numB = 0;
+                switch (Number(strAry[0])) {
+                    case 1713: {
+                        numB = 53;
+                        numA = 108;
+                        break;
+                    }
+                    case 1536: {
+                        numB = 73;
+                        numA = 108;
+                        break;
+                    }
+                    case 0: {
+                        numB = 1;
+                        numA = 256;
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+                if (numA == 256) {
+                    of = stC + of;
+                    of = stB * numA + of;
+                    if (of < 0) {
+                        break;
+                    }
+                    let pgA = parseInt(of / numA / numB);
+                    let pgC = parseInt(of % numA);
+                    ret = `${strAry[0]}.${strAry[1]}.${pgA}.${pgC}`;
+                } else {
+                    of = stC + of;
+                    of = stB * numA + of;
+                    of = stA * numB * numA + of;
+                    if (of < 0) {
+                        break;
+                    }
+                    let pgA = parseInt( of / numA / numB);
+                    let pgB = parseInt((of / numA) % numB);
+                    let pgC = parseInt( of % numA);
+                    ret = `${strAry[0]}.${pgA}.${pgB}.${pgC}`;
+                }
+            }
+        } else {
+            ret = "";
+        }
+        return ret;
+    }
 }
 
 export class ContextConfig {
@@ -58,7 +161,7 @@ export class ContextConfig {
      * @param {number} drawCount
      */
     constructor(lineWidth, lineJoin, lineCap, drawCount) {
-        this.lineWidth = lineWidth ?? 1;
+        this.lineWidth = lineWidth ?? 2;
         this.lineJoin = lineJoin ?? "round";
         this.lineCap = lineCap ?? "round";
         this.drawCount = drawCount ?? 2;
@@ -85,12 +188,14 @@ export class Canvas {
      * @param {string} address
      * @param {number} scale
      */
-    constructor(id, config, isRemote, address, scale) {
+    constructor(id, config, isRemote, address, scale, penSerial, pageNum) {
         this.id = id ?? GUID.NewGuid();
         this.config = config ?? new ContextConfig();
         this.isRemote = isRemote ?? false;
         this.address = address;
         this.setScale(scale ?? 1.5);
+        this.penSerial = penSerial;
+        this.pageNum = pageNum;
         this.lastPoint = null;
         /**
          * @type {Array<Array<Dot>>}
@@ -108,18 +213,37 @@ export class Canvas {
         return Stroke.PointsList2SVGList(this.points);
     }
     /**
+     * @param {Number} width
+     */
+    set strokeWidth(width) {
+        this.config.drawWidth = width;
+    }
+    /**
      *
      * @param {number} scale
      * @param {HTMLCanvasElement} canvas
      */
     setScale(scale, canvas) {
         this.scale = scale;
-        this.drawHeight = 360 * scale;
-        this.drawWidth = 960 * scale;
+        this.drawHeight = 940 * scale;
+        this.drawWidth = 1460 * scale;
         if (canvas) {
             canvas.width = this.drawWidth;
             canvas.height = this.drawHeight;
         }
+    }
+    /**
+     *
+     * @param {Dot} dot
+     * @param {HTMLCanvasElement} canvas
+     */
+    resetDot(dot, canvas) {
+        if (!dot.IsMove) return dot;
+        var x = (canvas.clientWidth / DotInfo.width) * dot.x * this.scale;
+        var y = (canvas.clientHeight / DotInfo.height) * dot.y * this.scale;
+        dot.x = x;
+        dot.y = y;
+        return dot;
     }
     bringToFront() {
         this.index = 80;
@@ -144,14 +268,17 @@ export class Canvas {
      */
     draw(dot, canvas = null) {
         if (dot.IsUp) {
-            if(this.lastPoint != null)
-                console.log(`UP : { x:${this.lastPoint.X},y:${this.lastPoint.Y} }`);
             this.lastPoint = null;
             var length = this.points.length;
             if (length == 0) return;
             if (this.points[length - 1].length == 0) return;
-            let s = Stroke.Points2SVG(this.points[length - 1]);
+            let s = new Stroke(
+                this.points[length - 1],
+                this.penSerial,
+                this.pageNum
+            );
             this.strokes.push(s);
+            console.log(s);
             this.points.push(new Array());
             return;
         }
@@ -175,10 +302,10 @@ export class Canvas {
         let controlPt = {};
         let control_scale = (0.8 / 0.5) * 0.175;
         controlPt.x =
-            this.lastPoint.x + (dot.x - this.lastPoint.x) * control_scale;
+            this.lastPoint.X + (dot.X - this.lastPoint.X) * control_scale;
         controlPt.y =
-            this.lastPoint.y + (dot.y - this.lastPoint.y) * control_scale;
-        context.quadraticCurveTo(controlPt.x, controlPt.y, dot.x, dot.y);
+            this.lastPoint.Y + (dot.Y - this.lastPoint.Y) * control_scale;
+        context.quadraticCurveTo(controlPt.x, controlPt.y, dot.X, dot.Y);
         this.lastPoint = dot;
         var t = 0;
         while (t < this.config.drawCount) {
@@ -186,19 +313,6 @@ export class Canvas {
             t++;
         }
         context.closePath();
-    }
-    /**
-     *
-     * @param {Dot} dot
-     * @param {HTMLCanvasElement} canvas
-     */
-    resetDot(dot, canvas) {
-        if (!dot.IsMove) return dot;
-        var x = (canvas.clientWidth / DotInfo.width) * dot.x * this.scale;
-        var y = (canvas.clientHeight / DotInfo.height) * dot.y * this.scale;
-        dot.x = x;
-        dot.y = y;
-        return dot;
     }
     /**
      *
@@ -215,6 +329,25 @@ export class Canvas {
         this.canvas = doc.getElementById(this.id);
         console.log(this.canvas ? "绑定成功" : "绑定失败");
     }
+    uploadStroke(activityId) {
+        if (this.strokes.length == 0) return Promise.resolve(true);
+        let strokes = this.strokes;
+        this.strokes = [];
+        let data = {
+            activityId: activityId,
+            pageNum: this.pageNum,
+            strokeList: strokes,
+        };
+        let r = Request.post("/signservice/stroke/uploadStroke", data);
+        console.log(data);
+        r.catch((e) => {
+            this.strokes.forEach((x) => {
+                strokes.push(x);
+            });
+            this.strokes = strokes;
+        });
+        return r;
+    }
 }
 
 export class Stroke {
@@ -222,9 +355,14 @@ export class Stroke {
      *
      * @param {Array<Dot>} points
      */
-    constructor(points) {
-        this.points = points;
-        this.path = Stroke.Points2SVG(points);
+    constructor(points, penSerial, pageNum, color = "#000") {
+        this.c = color;
+        this.p = Stroke.Points2SVG(points);
+        this.s = penSerial;
+        this.n = pageNum;
+        this.st = Timer.timeStamp();
+        this.x1 = points[0].X;
+        this.y1 = points[0].Y;
     }
 
     /**
@@ -232,15 +370,15 @@ export class Stroke {
      * @param {string} svg
      * @return {Array<Dot>}
      */
-    static SVG2Points(svg) {
+    static SVG2Points(svg,address = null) {
         let ret = new Array();
         if (svg == null || typeof svg != "string") return ret;
         let field = svg.split("l");
         if (field.length != 2) return ret;
         var firstPontArray = field[0].replace("M", "").split(" ");
-        let firstX = int.Parse(firstPontArray[0].trim());
-        let firstY = int.Parse(firstPontArray[1].trim());
-        ret.Add(new Dot(firstX, firstY));
+        let firstX = parseInt(firstPontArray[0].trim());
+        let firstY = parseInt(firstPontArray[1].trim());
+        ret.push(new Dot(firstX, firstY));
 
         var pointsArray = field[1]
             .replace("l", "")
@@ -249,9 +387,9 @@ export class Stroke {
             .split(" ");
         for (let i = 0; i < pointsArray.length; i++) {
             if (i + 1 < pointsArray.length) {
-                firstX = firstX + int.Parse(pointsArray[i].trim());
-                firstY = firstY + int.Parse(pointsArray[i + 1].trim());
-                ret.Add(new Dot(firstX, firstY));
+                firstX = firstX + parseInt(pointsArray[i].trim());
+                firstY = firstY + parseInt(pointsArray[i + 1].trim());
+                ret.push(Dot.Move(firstX, firstY,address));
             }
             i++;
         }
