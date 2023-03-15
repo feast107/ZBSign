19f3214691e68925f4bd2cbad43302405a1cf2be
Feast <1353216996@qq.com>
2023-03-15 20:27:48 +0800

    Update Format.js

--- src/utils/Format.js
+++ src/utils/Format.js
@@ -7,3 +7,10 @@ export const checkImageType = (type) => {
     var reg = /(png|jpg)$/
     return reg.test(type)
 }
+
+export class Timer{
+    static year = (timeStamp) => new Date(Number(timeStamp)).getFullYear() 
+    static month = (timeStamp) => new Date(Number(timeStamp)).getMonth() 
+    static day = (timeStamp) => new Date(Number(timeStamp)).getDay() 
+    static timeStamp(date){ date??=new Date(); return date - new Date(1970,1,1);  }
+}
