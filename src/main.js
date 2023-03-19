4de6b2c3367a161ec99638cf9287b70ce01e5cf7
Feast <1353216996@qq.com>
2023-03-19 12:41:57 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import VueCropper from 'vue-cropper'
-import 'vue-cropper/dist/index.css'
 import 'ideal-image-slider/ideal-image-slider.css'
 import 'ideal-image-slider/themes/default/default.css'
 import { ElMessage } from 'element-plus'
@@ -16,6 +14,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 app.config.unwrapInjectedRef = true
 app.use(ElementPlus)
-app.use(VueCropper)
 app.$message = ElMessage;
 app.mount('#app')
