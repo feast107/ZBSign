255fba1e227f1c76c4dd2cdc571a6dfcec7511fe
Feast <1353216996@qq.com>
2023-03-19 22:35:00 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+    chainWebpack: config => {
+        config.plugin('html').tap(
+            args => {
+                debugger;
+                args[0].title = '孜博汇签到程序';
+                return args;
+            }
+        )
+    },
     transpileDependencies: true,
     pluginOptions: {
         electronBuilder: {
+            name: "孜博汇签到程序",
+            appId: "com.zbform.sign",
+            productName: "孜博汇签到程序",
             preload: "src/preload.js",
-            chainWebpackMainProcess : (_) => "src/background.js",
+            chainWebpackMainProcess: (_) => { },
         },
     },
 });
