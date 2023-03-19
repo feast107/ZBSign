087efbf26e7c61e90582737a215cfd97e942f4fc
Feast <1353216996@qq.com>
2023-03-19 22:34:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ let app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+
 app.config.unwrapInjectedRef = true
 app.use(ElementPlus)
 app.$message = ElMessage;
