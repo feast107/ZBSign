095e128db5729a331d8e1e74865292f547f89cd1
Feast <1353216996@qq.com>
2023-03-21 18:10:10 +0800

    Update Global.js

--- src/utils/Global.js
+++ src/utils/Global.js
@@ -3,7 +3,7 @@ export default (function () {
         Object.keys(from).forEach((k) => {
             to[k] = from[k];
         });
-    }
+    };
     /**
      * Remove specified target
      * @param {Any} item
@@ -12,31 +12,43 @@ export default (function () {
         let index = this.findIndex((x) => Object.is(x, item));
         return index >= 0 ? this.splice(index, 1)[0] : false;
     };
-    Object.defineProperty(Object.prototype, 'copy',
-        {
-            get: function () {
-                let ret;
-                if (this instanceof Array) {
-                    ret = new Array();
-                    this.forEach((x) => {
-                        ret.push(x);
-                    });
-                } else if (this instanceof Map) {
-                    ret = new Map();
-                    let tmp = Object.fromEntries(this);
-                    Object.keys(tmp).forEach(k => {
-                        ret.set(k, tmp[k]);
-                    })
-                } else {
-                    ret = new Object();
-                    ret.__proto__ = this.__proto__;
-                }
-                copy(this, ret);
-                return ret;
-            },
-            set: (_) => { }
-        }
-    )
+    Object.defineProperty(Object.prototype, "copy", {
+        get: function () {
+            let ret;
+            if (this instanceof Array) {
+                ret = new Array();
+                this.forEach((x) => {
+                    ret.push(x);
+                });
+            } else if (this instanceof Map) {
+                ret = new Map();
+                let tmp = Object.fromEntries(this);
+                Object.keys(tmp).forEach((k) => {
+                    ret.set(k, tmp[k]);
+                });
+            } else {
+                ret = new Object();
+                ret.__proto__ = this.__proto__;
+            }
+            copy(this, ret);
+            return ret;
+        },
+        set: (_) => {},
+    });
+    Object.defineProperty(Object.prototype, "proxy", {
+        get: function () {
+            const handler = {
+                get: function () {
+                    return Reflect.get(...arguments);
+                },
+                set: function () {
+                    return Reflect.set(...arguments);
+                },
+            };
+            return new Proxy(this, handler);
+        },
+        set: (_) => {},
+    });
     Date.prototype.timeStamp = function () {
         return this - new Date(1970);
     };
@@ -48,19 +60,21 @@ export default (function () {
                 let getter = {
                     get: function () {
                         return this.code == 1;
-                    }
+                    },
                 };
-                Object.defineProperty(ret, "Success", getter)
-                Object.defineProperty(ret, "success", getter)
-              }
+                Object.defineProperty(ret, "Success", getter);
+                Object.defineProperty(ret, "success", getter);
+            }
             return ret;
         } catch (e) {
             return {
-                get Success() { return false; },
+                get Success() {
+                    return false;
+                },
                 code: 0,
                 msg: e,
                 data: null,
-            }
+            };
         }
-    }
+    };
 })();
