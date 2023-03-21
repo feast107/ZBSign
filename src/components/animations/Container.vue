9ea60c00b25b77a8c368bbc6753e3e657d5ac2b9
Feast <1353216996@qq.com>
2023-03-21 14:28:50 +0800

    Create Container.vue

---
+++ src/components/animations/Container.vue
@@ -0,0 +1,52 @@
+<template>
+    <div
+        :style="`display:${can.display};background-image:url(${activity.rightBorder}); `"
+        :class="can.className">
+        <canvas
+            :id="can.id"
+            :z-index="can.index"
+            class="innerCanvas"
+            :style="`z-index:30;background-color:#aaaa0040`"
+            :width="can.drawWidth"
+            :height="can.drawHeight">
+        </canvas>
+        <div
+            :style="`background-image:url(${activity.pageUrl});z-index:20`"
+            class="innerCanvas"></div>
+    </div>
+</template>
+
+<script>
+import { ComponentKey } from "@/utils/Definition";
+import { Activity } from "@/utils/Activity";
+import { Canvas } from "@/utils/Canvas";
+export default {
+    props: ["canvas"],
+    inject: [ComponentKey.PlayActicity, ComponentKey.Dotpen],
+    data() {
+        return {
+            /**
+             * @type {Activity}
+             */
+            activity: this[ComponentKey.PlayActicity],
+            /**
+             * @type {Canvas}
+             */
+            can: this["canvas"],
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+.innerCanvas {
+    left: 0;
+    position: absolute;
+    width: calc(100% - 100px);
+    height: calc(100% - 100px);
+    margin: 50px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+</style>
