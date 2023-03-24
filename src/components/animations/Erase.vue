60c893be0ab756e5d4ae2b8d8f3b769f6c763bfb
Feast <1353216996@qq.com>
2023-03-24 18:12:10 +0800

    Create Erase.vue

---
+++ src/components/animations/Erase.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { ComponentKey } from '@/utils/Definition';
+export default {
+    inject: [ComponentKey.ModifingActivity],
+    data() {
+        return {
+            activity: this[ComponentKey.ModifingActivity],
+            regions: [],
+        }
+    },
+    created() { },
+    unmounted() { }
+}
+</script>
+<style></style>
