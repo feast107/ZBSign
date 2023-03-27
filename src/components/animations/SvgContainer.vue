f099a6e81023284f0129ead20805c793b57864e4
Feast <1353216996@qq.com>
2023-03-27 18:25:38 +0800

    Create SvgContainer.vue

---
+++ src/components/animations/SvgContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import { ComponentKey } from '@/utils/Definition';
+export default {
+    inject: [ComponentKey.ModifingActivity],
+    data() {
+        return {
+            activity: this[ComponentKey.ModifingActivity],
+            regions: [],
+            svgs:[]
+        }
+    },
+    created() { },
+    unmounted() { }
+}
+</script>
+<style>
+</style>
