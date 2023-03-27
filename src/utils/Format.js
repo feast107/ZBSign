330f19dc998980a0b303ba702f672185c1d44cee
Feast <1353216996@qq.com>
2023-03-27 18:25:54 +0800

    Update Format.js

--- src/utils/Format.js
+++ src/utils/Format.js
@@ -12,5 +12,5 @@ export class Timer{
     static year = (timeStamp) => new Date(Number(timeStamp)).getFullYear() 
     static month = (timeStamp) => new Date(Number(timeStamp)).getMonth() 
     static day = (timeStamp) => new Date(Number(timeStamp)).getDay() 
-    static timeStamp(date){ date??=new Date(); return date - new Date(1970,1,1);  }
+    static timestamp(date){ date??=new Date(); return date.timestamp;  }
 }
