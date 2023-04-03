dd265fdf277760f343caa4404d7668bfa8d62d34
Feast <1353216996@qq.com>
2023-04-03 18:15:06 +0800

    Create Aspratio.vue

---
+++ src/components/layout/Aspratio.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="width: 100%; height: 100%;">
+        <div :style="wrapper">
+            <div style="position: absolute;top: 0; right: 0; bottom: 0; left: 0 ">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        ratio: {
+            type: Number,
+            default: 1,
+        },
+        direction: {
+            type: String,
+            default: "top",
+        }
+    },
+    data() {
+        return {
+            wrapper: `overflow: hidden;position:relative;height: 0; width: 100%; padding: 0;padding-${this.direction}: calc(${this.ratio} * 100%);`,
+        }
+    }
+};
+</script>
+
+<style></style>
