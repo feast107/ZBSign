b100ed9e2988e4f7de70a15ca9822ef6bab3978a
Feast <1353216996@qq.com>
2023-04-04 18:16:12 +0800

    Update Aspratio.vue

--- src/src/components/layout/Aspratio.vue
+++ src/src/components/layout/Aspratio.vue
@@ -1,6 +1,8 @@
 <template>
-    <div style="width: 100%; height: 100%;">
-        <div :style="wrapper">
+    <div ref="outer"
+        :style="`width:${width}%; height: 100%;margin-top:${marginVertical}px;margin-left:${marginHorizen}px;margin-right:${marginHorizen}px;`">
+        <div
+            :style="`overflow: hidden;position:relative;height: 0; width: 100%; padding: 0;padding-${this.direction}: calc(${this.ratio} * 100%);`">
             <div style="position: absolute;top: 0; right: 0; bottom: 0; left: 0 ">
                 <slot></slot>
             </div>
@@ -9,7 +11,6 @@
 </template>
 
 <script>
-
 export default {
     props: {
         ratio: {
@@ -19,11 +20,39 @@ export default {
         direction: {
             type: String,
             default: "top",
-        }
+        },
+    },
+
+    mounted() {
+        this.resize();
+        window.addEventListener("resize", this.resize);
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.resize);
     },
     data() {
         return {
-            wrapper: `overflow: hidden;position:relative;height: 0; width: 100%; padding: 0;padding-${this.direction}: calc(${this.ratio} * 100%);`,
+            marginVertical: 0,
+            marginHorizen: 0,
+            width: 100,
+        }
+    },
+    methods: {
+        resize() {
+            let parent = this.$refs.outer.parentElement;
+            let pRect = parent.getBoundingClientRect();
+            let pw = pRect.width;
+            let ph = pRect.height;
+            let desiredWidth = 0;
+            let desiredHeight = pw * this.ratio;
+            if (desiredHeight > ph) {
+                desiredHeight = ph;
+                desiredWidth = desiredHeight / this.ratio;
+                this.marginHorizen = (pw - desiredWidth) / 2;
+                this.width = 100 * desiredWidth / pw;
+            } else {
+                this.marginVertical = (ph - desiredHeight) / 2;
+            }
         }
     }
 };
