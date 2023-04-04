8b0dfef0d86c52b0fb96d17810f1718738e06aa9
Feast <1353216996@qq.com>
2023-04-04 18:15:57 +0800

    Update Container.vue

--- src/src/components/animations/Container.vue
+++ src/src/components/animations/Container.vue
@@ -1,15 +1,15 @@
 <template>
-    <Aspratio :ratio="0.66" :class="can.className">
-        <MultipleBorder :top="20" :bottom="20" :left="14" :right="14" :base-url="activity.rightBorder">
-            <template #center>
-                <div :style="`width:100%;height:100%;display:${can.display};background-image:url(${activity.pageUrl});`">
-                    <canvas :id="can.id" :z-index="can.index" class="innerCanvas"
-                        :style="`z-index:30;background-color:#aaaa0040`" :width="can.drawWidth" :height="can.drawHeight">
-                    </canvas>
-                </div>
-            </template>
-        </MultipleBorder>
-    </Aspratio>
+    <Anoaspratio :ratio="0.72" :class="can.className">
+        <div style="position:absolute;width:100%;height:100%;background-color: rebeccapurple;z-index: 0;"></div>
+        <Ascaler :horizontal="82" :vertical="78.6" style="position:absolute">
+            <div class="innerCanvas"
+                :style="`width:100%;height:100%;display:${can.display};background-image:url(${activity.pageUrl});background-size: contain;`">
+                <canvas :id="can.id" :z-index="can.index" class="innerCanvas"
+                    :style="`z-index:30;background-color:#aaaa0040`" :width="can.drawWidth" :height="can.drawHeight">
+                </canvas>
+            </div>
+        </Ascaler>
+    </Anoaspratio>
 </template>
 
 <script>
@@ -18,6 +18,8 @@ import { Activity } from "@/utils/Activity";
 import { Canvas } from "@/utils/Canvas";
 import Aspratio from "../layout/Aspratio.vue";
 import MultipleBorder from "./MultipleBorder.vue";
+import Anoaspratio from "../layout/Anoaspratio.vue";
+import Ascaler from "../layout/Ascaler.vue";
 export default {
     props: ["canvas"],
     inject: [ComponentKey.PlayActicity, ComponentKey.Dotpen],
@@ -34,15 +36,19 @@ export default {
         };
     },
     methods: {},
-    components: { Aspratio, MultipleBorder }
+    components: { Aspratio, MultipleBorder, Anoaspratio, Ascaler }
 };
 </script>
 
 <style lang="scss">
+.asp {
+    position: absolute;
+}
+
 .innerCanvas {
     width: 100%;
-    height:  100%;
-    background-size: cover;
+    height: 100%;
+    background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
 }
