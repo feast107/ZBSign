2e59b2a008a62cfc4b3632cf71e36133e9dcdf32
Feast <1353216996@qq.com>
2023-04-06 16:23:59 +0800

    Update Container.vue

--- src/src/components/animations/Container.vue
+++ src/src/components/animations/Container.vue
@@ -1,13 +1,11 @@
 <template>
-    <Anoaspratio :ratio="0.72" :class="can.className">
+    <Anoaspratio :ratio="0.72" :class="can.className" :style="`display:${can.display}`">
         <div class="rightBorder" :style="`background-image:url(${activity.rightBorder});z-index: 500;`">
         </div>
         <Ascaler :horizontal="82" :vertical="78.6">
             <div class="innerCanvas"
                 :style="`width:100%;height:100%;display:${can.display};background-image:url(${activity.pageUrl});background-size: contain;`">
-                <canvas :id="can.id" :z-index="can.index" class="innerCanvas"
-                    :style="`z-index:30;background-color:#aaaa0040`" :width="can.drawWidth" :height="can.drawHeight">
-                </canvas>
+                <SvgCanvas :strokes="can.strokes"></SvgCanvas>
             </div>
         </Ascaler>
     </Anoaspratio>
@@ -21,6 +19,7 @@ import Aspratio from "../layout/Aspratio.vue";
 import MultipleBorder from "./MultipleBorder.vue";
 import Anoaspratio from "../layout/Anoaspratio.vue";
 import Ascaler from "../layout/Ascaler.vue";
+import SvgCanvas from "./SvgCanvas.vue";
 export default {
     props: ["canvas"],
     inject: [ComponentKey.PlayActicity, ComponentKey.Dotpen],
@@ -37,7 +36,11 @@ export default {
         };
     },
     methods: {},
-    components: { Aspratio, MultipleBorder, Anoaspratio, Ascaler }
+    components: { Aspratio, MultipleBorder, Anoaspratio, Ascaler, SvgCanvas },
+    created() {
+        this.canvas;
+        this.can;
+    }
 };
 </script>
 
