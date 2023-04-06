56c44e75e498798d2774caf599cfb870fba1e87d
Feast <1353216996@qq.com>
2023-04-06 16:24:05 +0800

    Create SvgCanvas.vue

---
+++ src/src/components/animations/SvgCanvas.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg viewBox="0 0 10000 6900" class="strokes" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <g fill="none">
+            <path v-for="stroke in strokes" :key="stroke" :d="stroke.p" :stroke="stroke.c" stroke-width="10">
+            </path>
+        </g>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: {
+        strokes: {
+            type: Array,
+            default: () => [],
+        }
+    }
+}
+</script>
