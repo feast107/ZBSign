f3abb69a7ab97f61258b2d04a820fcce43c4a80f
Feast <1353216996@qq.com>
2023-04-07 13:49:14 +0800

    Delete DictSelector.vue

---
+++ src/src/components/controls/PopSelector.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-popover @show="show" placement="right" :width="400" trigger="click">
+        <template #reference>
+            <el-button style="padding: 0; border: none">
+                <img style="width: 30px; height: 30px" :src="src" />
+            </el-button>
+        </template>
+        <el-selector>
+            
+        </el-selector>
+        <slot ref="selector"></slot>
+    </el-popover>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+}
+</script>
